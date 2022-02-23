bc6f22278e90f877ac51beabbcc47f5809772473
suhasgund10 <suhasgund10@gmail.com>
2022-02-23 17:06:40 +0530

    My First JS commit

---
+++ FindUniqueElements.js
@@ -0,0 +1,24 @@
+function getElementIndex(arr, ele) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === ele) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+function printUniqueElements(ip) {
+  let op = [];
+  for (let i = 0; i < ip.length; i++) {
+    index = getElementIndex(op, ip[i]);
+    if (index === -1) {
+      op.push(ip[i]);
+    }
+  }
+
+  console.log("Unique numbers:" + op);
+}
+
+a = [2, 14, 34, 56, 12, 2, 14, 34, 6, 8, 7, 4, 12];
+printUniqueElements(a);
