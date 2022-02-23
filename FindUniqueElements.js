f357261459ba1180ddd38cc32692c18575f5298d
suhasgund10 <suhasgund10@gmail.com>
2022-02-23 17:14:22 +0530

    This is my second test commit.

--- FindUniqueElements.js
+++ FindUniqueElements.js
@@ -17,8 +17,11 @@ function printUniqueElements(ip) {
     }
   }
 
-  console.log("Unique numbers:" + op);
+  console.log("Unique numbers are:" + op);
 }
 
 a = [2, 14, 34, 56, 12, 2, 14, 34, 6, 8, 7, 4, 12];
 printUniqueElements(a);
+
+b = [56, 12, 2, 14, 34, 55, 67, 89];
+printUniqueElements(b);
